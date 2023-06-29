3b029dd48c46f0c64c85cf66c654d534c6526455
William Tran <wtran29@gmail.com>
2023-06-28 17:56:52 -0700

    added middleware call to ping and make sure server is live, included routes to authenicate and handle submission

--- auth-service/cmd/api/routes.go
+++ auth-service/cmd/api/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 )
@@ -19,5 +20,10 @@ func (app *Config) routes() http.Handler {
 		AllowCredentials: true,
 		MaxAge:           300,
 	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Post("/authenticate", app.Authenticate)
+	mux.Post("/handle", app.HandleSubmission)
 	return mux
 }
